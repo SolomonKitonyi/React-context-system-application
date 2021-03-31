9148deb58598c0a75f541d1f3b5de85c2fb5565b
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-31 13:38:43 +0300

    add changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const App = ()=>{
-    return <div>App</div>
-};
+class App extends React.Component{
+    render(){
+        return <div className="ui container">App</div>
+    };
+}
 
 export default App;
